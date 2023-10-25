f1aed8b64c717dd0185226a8d771803c1aa35b3f
ReynerCr <25756546+ReynerCr@users.noreply.github.com>
2023-10-25 15:46:43 -0400

    Presentación final

--- src/AG.java
+++ src/AG.java
@@ -9,15 +9,89 @@ public class AG {
     private ArrayList<Double> Covarianza;
     private Double[][] MatrizCovarianza;
     private ArrayList<Double> fitness;
-
     private ArrayList<ArrayList<Double>> Ganadores;
     private ArrayList<ArrayList<Double>> NuevaGeneracion;
+    private int tamanioPoblacion;
 
     // Constructor
-    public AG(ArrayList<Coin> Coins, Double[][] MatrizCovarianza) {
+    public AG(ArrayList<Coin> Coins, Double[][] MatrizCovarianza, int tamanioPoblacion) {
         this.Coins = Coins;
         this.MatrizCovarianza = MatrizCovarianza;
+        this.tamanioPoblacion = tamanioPoblacion;
     }
+
+    // * Funciones para la funcion de fitness
+
+    // Funcion para crear los numeros aleatorios para pesos
+    public void CrearNumeros() {
+        Random random = new Random();
+        Pesos = new ArrayList<ArrayList<Double>>();
+
+        for (int i = 0; i < tamanioPoblacion; i++) {
+            ArrayList<Double> list = new ArrayList<>(); // cada gen de cada cromosoma
+            Double acum = 0.0d;
+            for (int j = 0; j < 10; j++) {
+                Double numero = random.nextDouble() * tamanioPoblacion;
+                acum = acum + numero;
+                list.add(numero);
+            }
+
+            // Normalizando los pesos
+            for (int j = 0; j < 10; j++) {
+                Double numero = list.get(j);
+                numero = numero / acum;
+                list.set(j, numero);
+            }
+
+            Pesos.add(list);
+        }
+    } // fin CrearNumeros
+
+    // Funcion para calcular la covarianza
+    public void CrearCovarianzas() {
+        Covarianza = new ArrayList<>();
+        for (int k = 0; k < tamanioPoblacion; k++) {
+            Double cova = 0.0;
+            for (int i = 0; i < 10; i++) {
+
+                for (int j = 0; j < 10; j++) {
+                    cova = cova + (Pesos.get(k).get(i) * Pesos.get(k).get(j) * MatrizCovarianza[i][j]);
+                }
+            }
+            Covarianza.add(cova);
+        }
+    } // fin CrearCovarianzas
+
+    // Funcion para calcular los rendimientos (funcion max)
+    public void CrearRendimientos() {
+        Rendimientos = new ArrayList<>();
+
+        for (int i = 0; i < tamanioPoblacion; i++) {
+            Double acumulador = 0.0d;
+
+            for (int j = 0; j < 10; j++) {
+                acumulador = acumulador + Pesos.get(i).get(j) * Coins.get(j).getMediaRpi();
+            }
+            Rendimientos.add(acumulador);
+        }
+    } // fin CrearRendimientos
+
+    // Funcion para calcular el fitness
+    public void Crearfitness() {
+        fitness = new ArrayList<>();
+
+        for (int i = 0; i < tamanioPoblacion; i++) {
+            fitness.add(Rendimientos.get(i) / Covarianza.get(i));
+        }
+    } // fin Crearfitness
+
+    // * Otras funciones que si son de AG
+
+    // Funcion para reemplazar la poblacion
+    public void ReemplazarPoblacion() {
+        Pesos = NuevaGeneracion;
+        NuevaGeneracion = new ArrayList<ArrayList<Double>>();
+    } // fin ReemplazarPoblacion
 
     public ArrayList<Double> getFitness() {
         return fitness;
@@ -34,27 +108,36 @@ public class AG {
         return Pesos;
     }
 
-    // Funcion para reemplazar la poblacion
-    public void ReemplazarPoblacion() {
-        Pesos = NuevaGeneracion;
-        NuevaGeneracion = new ArrayList<ArrayList<Double>>();
-    } // fin ReemplazarPoblacion
+    // Funcion para el cruce de cromosomas con pesos
+    public ArrayList<ArrayList<Double>> crucePorPesos(ArrayList<Double> padre1, ArrayList<Double> padre2) {
+        ArrayList<ArrayList<Double>> hijos = new ArrayList<ArrayList<Double>>();
+        hijos.add(new ArrayList<Double>());
+        hijos.add(new ArrayList<Double>());
+        Random rand = new Random();
 
-
-    // Funcion para calcular la covarianza
-    public void CrearCovarianzas() {
-        Covarianza = new ArrayList<>();
-        for (int k = 0; k < 100; k++) {
-            Double cova = 0.0;
-            for (int i = 0; i < 10; i++) {
-
-                for (int j = 0; j < 10; j++) {
-                    cova = cova + (Pesos.get(k).get(i) * Pesos.get(k).get(j) * MatrizCovarianza[i][j]);
-                }
-            }
-            Covarianza.add(cova);
+        Double acum1 = 0.0d;
+        Double acum2 = 0.0d;
+        Double aux = rand.nextDouble();
+        for (int i = 0; i < padre1.size(); i++) {
+            hijos.get(0).add(aux * padre1.get(i) + (1 - aux) * padre2.get(i));
+            hijos.get(1).add(aux * padre2.get(i) + (1 - aux) * padre1.get(i));
+            acum1 += hijos.get(0).get(i);
+            acum2 += hijos.get(1).get(i);
         }
-    } // fin CrearCovarianzas
+
+        return hijos;
+    } // fin crucePorPesos
+
+    // Funcion para la mutacion de cromosomas
+    public void mutacion(ArrayList<Double> cromosoma) {
+        // Intercambia de posicion los cromosomas
+        Random rand = new Random();
+        int pos1 = rand.nextInt(10);
+        int pos2 = rand.nextInt(10);
+        Double aux = cromosoma.get(pos1);
+        cromosoma.set(pos1, cromosoma.get(pos2));
+        cromosoma.set(pos2, aux);
+    } // fin mutacion
 
     // Funcion para realizar el torneo
     public void Torneo() {
@@ -65,7 +148,7 @@ public class AG {
 
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 20; j++) {
-                int numero = random.nextInt(100);
+                int numero = random.nextInt(tamanioPoblacion);
                 Indices.add(numero);
                 Participantes.add(fitness.get(Indices.get(j)));
             }
@@ -84,90 +167,6 @@ public class AG {
     public ArrayList<ArrayList<Double>> GetNuevaGeneracion() {
         return NuevaGeneracion;
     }
-    // Funcion para calcular el fitness
-    public void Crearfitness() {
-        fitness = new ArrayList<>();
-
-        for (int i = 0; i < 100; i++) {
-            fitness.add(Rendimientos.get(i) / Covarianza.get(i));
-        }
-    } // fin Crearfitness
-
-    // Funcion para calcular los rendimientos (funcion max)
-    public void CrearRendimientos() {
-        Rendimientos = new ArrayList<>();
-
-        for (int i = 0; i < 100; i++) {
-            Double acumulador = 0.0d;
-
-            for (int j = 0; j < 10; j++) {
-                acumulador = acumulador + Pesos.get(i).get(j) * Coins.get(j).getMediaRpi();
-            }
-            Rendimientos.add(acumulador);
-        }
-    } // fin CrearRendimientos
-
-    // Funcion para crear los numeros aleatorios para pesos
-    public void CrearNumeros() {
-        Random random = new Random();
-        Pesos = new ArrayList<ArrayList<Double>>();
-
-        int M = 100;
-        for (int i = 0; i < M; i++) {
-            ArrayList<Double> list = new ArrayList<>(); // cada gen de cada cromosoma
-            Double acum = 0.0d;
-            for (int j = 0; j < 10; j++) {
-                Double numero = random.nextDouble() * 100;
-                acum = acum + numero;
-                list.add(numero);
-            }
-
-            for (int j = 0; j < 10; j++) {
-                Double numero = list.get(j);
-                numero = numero / acum;
-                list.set(j, numero);
-            }
-
-            Pesos.add(list);
-        }
-    } // fin CrearNumeros
-
-    // Funcion para el cruce de cromosomas con pesos
-    public ArrayList<ArrayList<Double>> crucePorPesos(ArrayList<Double> padre1, ArrayList<Double> padre2) {
-        ArrayList<ArrayList<Double>> hijos = new ArrayList<ArrayList<Double>>();
-        hijos.add(new ArrayList<Double>());
-        hijos.add(new ArrayList<Double>());
-        Random rand = new Random();
-
-        Double acum1 = 0.0d;
-        Double acum2 = 0.0d;
-        for (int i = 0; i < padre1.size(); i++) {
-            Double aux = rand.nextDouble();
-            hijos.get(0).add(aux * padre1.get(i) + (1 - aux) * padre2.get(i));
-            hijos.get(1).add(aux * padre2.get(i) + (1 - aux) * padre1.get(i));
-            acum1 += hijos.get(0).get(i);
-            acum2 += hijos.get(1).get(i);
-        }
-
-        // Re normalizar los pesos
-        for (int j = 0; j < 10; j++) {
-            hijos.get(0).set(j, hijos.get(0).get(j) / acum1);
-            hijos.get(1).set(j, hijos.get(1).get(j) / acum2);
-        }
-
-        return hijos;
-    } // fin crucePorPesos
-
-    // Funcion para la mutacion de cromosomas
-    public void mutacion(ArrayList<Double> cromosoma) {
-        // Intercambia de posicion los cromosomas
-        Random rand = new Random();
-        int pos1 = rand.nextInt(10);
-        int pos2 = rand.nextInt(10);
-        Double aux = cromosoma.get(pos1);
-        cromosoma.set(pos1, cromosoma.get(pos2));
-        cromosoma.set(pos2, aux);
-    } // fin mutacion
 
     public void setMatrizCovarianzas(Double[][] MatrizCovarianza) {
         this.MatrizCovarianza = MatrizCovarianza;
--- src/App.java
+++ src/App.java
@@ -1,17 +1,15 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
 import java.util.Collections;
 
 public class App {
     public static void main(String[] args) throws Exception {
         // para la cantidad de meses a leer por archivo
-
-        ArrayList<Coin> cryptos = null;
         // 60 meses = 5 anios
         int cantMeses = 60;
+
+        ArrayList<Coin> cryptos = null;
 
         try {
             ParseCSV csv = new ParseCSV("./prices", cantMeses);
@@ -25,86 +23,114 @@ public class App {
         // Calculo de la matriz de covarianza de los rendimientos
         List<List<Double>> Acum = new ArrayList<>();
 
+        // Acum es una lista de lista de lista de vector (rpi-media)
         for (Coin crypto : cryptos) {
             Double[] vector2 = crypto.getvector_rpi_m();
-
             List<Double> conjunto = new ArrayList<>();
             conjunto = Arrays.asList(vector2);
             Acum.add(conjunto);
         }
 
+        // Se calcula multiplicacion de cada i, k de Acum
+        // Y se saca la media
         Double[][] MatrizCovarianza = new Double[10][10];
-
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
                 Double acum = 0.0d;
-                for (int k = 0; k < 60; k++) {
-                    // Aqui fue el error
+                for (int k = 0; k < cantMeses; k++) {
                     acum = acum + (Acum.get(i).get(k) * Acum.get(j).get(k));
                 }
-                acum = acum / 60;
-
+                acum = acum / cantMeses;
                 MatrizCovarianza[i][j] = acum;
             }
         }
 
-        AG ag = new AG(cryptos, MatrizCovarianza);
+        int tamanioPoblacion = 100;
+        int tamanioIteraciones = 30;
+        int iter = 0;
 
-        ag.CrearNumeros();
-        ag.CrearRendimientos();
-        ag.CrearCovarianzas();
-        ag.Crearfitness();
-
-        for (int i = 0; i < 30; i++) { // 30 iteraciones
-            while (ag.getNuevaPoblación() != 100) { // 100 individuos
-                ag.Torneo(); // torneo
-                // Calculo random para cruza y muta
-                Double prueba_cruce = Math.random();
-                Double prueba_muta = Math.random();
-                // Ganadores de torneo
-                ArrayList<ArrayList<Double>> Ganadores = new ArrayList<ArrayList<Double>>();
-                if (prueba_cruce <= 0.80) { // cruza
-                    Ganadores = ag.GetGanadores();
-                    Ganadores = ag.crucePorPesos(Ganadores.get(0), Ganadores.get(1));
-                }
-                if (prueba_muta <= 0.05) { // muta
-                    for (int k = 0; k < Ganadores.size(); k++) {
-                        ag.mutacion(Ganadores.get(k));
-                    }
-                }
-
-                // Aniado los ganadores a la nueva poblacion
-                for (int k = 0; k < Ganadores.size(); k++) {
-                    ag.GetNuevaGeneracion().add(Ganadores.get(k));
-                }
-            } // while para la poblacion
-
-            // Reemplazo la poblacion
-            ag.ReemplazarPoblacion();
-            // Calculo de fitness de la nueva poblacion
+        while (iter < tamanioIteraciones) { // iteraciones de AG distintas con soluciones distintas
+            AG ag = new AG(cryptos, MatrizCovarianza, tamanioPoblacion);
+            ag.CrearNumeros();
             ag.CrearRendimientos();
             ag.CrearCovarianzas();
             ag.Crearfitness();
-        } // fin for de las iteraciones generaciones
+            for (int i = 0; i < 30; i++) { // 30 iteraciones o generaciones
+                // Seleccion de nueva poblacion
+                // Se ejecuta hasta tener los tamanioPoblacion individuos
+                while (ag.getNuevaPoblación() != tamanioPoblacion) {
+                    ag.Torneo(); // torneo
+                    // Calculo random para cruza y muta
+                    Double prueba_cruce = Math.random();
+                    Double prueba_muta = Math.random();
+                    // Ganadores de torneo
+                    ArrayList<ArrayList<Double>> Ganadores = new ArrayList<ArrayList<Double>>();
+                    if (prueba_cruce <= 0.80) { // cruza
+                        Ganadores = ag.GetGanadores();
+                        Ganadores = ag.crucePorPesos(Ganadores.get(0), Ganadores.get(1));
+                    }
+                    if (prueba_muta <= 0.05) { // muta
+                        for (int k = 0; k < Ganadores.size(); k++) {
+                            ag.mutacion(Ganadores.get(k));
+                        }
+                    }
+                    // Aniado los ganadores a la nueva poblacion
+                    for (int k = 0; k < Ganadores.size(); k++) {
+                        ag.GetNuevaGeneracion().add(Ganadores.get(k));
+                    }
+                } // while para la poblacion
 
-        // Calculo de la mejor solucion, fitness y peso
-        Double mejor = Collections.max(ag.getFitness());
-        Integer index = ag.getFitness().indexOf(mejor);
+                // Reemplazo la poblacion
+                ag.ReemplazarPoblacion();
+                // Calculo de fitness de la nueva poblacion
+                ag.CrearRendimientos();
+                ag.CrearCovarianzas();
+                ag.Crearfitness();
+            } // fin for de las iteraciones generaciones
 
-        // Tengo que imprimir todo con los datos de Coin
-        ArrayList<Double> pesos = ag.getPesos().get(index);
-        
-        System.out.println("Mejor solucion: " + pesos);
-        System.out.println("Mejor fitness: " + ag.getFitness().get(index));
-        
-        int [] coins_index = new int[cryptos.size()];
-        for (int i = 0; i < cryptos.size(); i++) {
-            coins_index[i] = cryptos.get(i).getIndex();
-        }
+            // Calculo de la mejor solucion, fitness y peso
+            Double mejor = Collections.max(ag.getFitness());
+            Integer index = ag.getFitness().indexOf(mejor);
+            ArrayList<Double> pesos = ag.getPesos().get(index);
 
-        System.out.println("Mejor combinacion de cryptos: ");
-        for (int i = 0; i < pesos.size(); i++) {
-            System.out.println(cryptos.get(coins_index[i]).getSymbol() + " " + pesos.get(i));
-        }
+            // imprimirResultadosExcel(pesos, mejor);
+            imprimirNormal(ag, cryptos, pesos, index);
+
+            ++iter; // aumentar iteracion
+        } // fin iteraciones
     } // fin main
+
+    // Funcion para imprimir resultados normal
+    public static void imprimirNormal(AG ag, ArrayList<Coin> cryptos, ArrayList<Double> pesos, Integer index) {
+        System.out.println("Fitness: " + ag.getFitness().get(index));
+        for (int i = 0; i < pesos.size(); i++) {
+            System.out.println(cryptos.get(i).getSymbol() + ": " + pesos.get(i));
+        }
+        System.out.println();
+    }
+
+    // Funcion para imprimir resultados para alimentar un Excel
+    public static void imprimirResultadosExcel(ArrayList<Double> pesos, Double fitness) {
+        // Truncar los pesos a 4 decimales
+        for (int i = 0; i < pesos.size(); i++) {
+            pesos.set(i, Math.floor(pesos.get(i) * 10000) / 10000);
+        }
+
+        // Truncar fitness a 6 decimales
+        fitness = Math.floor(fitness * 1000000) / 1000000;
+
+        // Convertir los pesos a string y reemplazar los puntos por comas e imprimir los
+        // pesos separados por espacios, quitando los caracteres [] de que era un array
+        String pesosString = pesos.toString().replace(",", "");
+        pesosString = pesosString.replace(".", ",");
+        pesosString = pesosString.replace("[", "");
+        pesosString = pesosString.replace("]", "");
+        System.out.print(pesosString);
+
+        System.out.print(" ");
+
+        // Imprimir el fitness reemplazando el punto por coma
+        String fitnessString = fitness.toString().replace(".", ",");
+        System.out.println(fitnessString);
+    } // fin imprimirResultados
 } // fin clase
